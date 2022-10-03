298f19f81b89052d3d75efe70e2458ec9a7fa066
vojaaja <vojislavmladenovic1@gmail.com>
2022-10-03 17:56:22 +0200

    poruka za commit

---
+++ contextmanager.py
@@ -0,0 +1,17 @@
+class Open_File():
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        self.file.close()
+
+with Open_File('sample.txt', 'w') as f:
+    f.write('Testing')
+
+print(f.closed)
